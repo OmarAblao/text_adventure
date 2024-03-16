f84b323e98f4146b456650fa82c738aa9f75ca90
OmarAblao <omar.rahman90@yahoo.com>
2024-03-15 20:15:11 -0700

    v1_outline

---
+++ main.py
@@ -0,0 +1,31 @@
+# def main():
+#     choice = input("What will you do? Fight or Flee?")
+#     if input == ("Fight"):
+#         print("You begin the ")
+
+
+
+#Welcome message
+print("Welcome to Adventure Text! Let's see if you can survive. ")
+
+#Ask for user's name and properly format
+name = input("Before we begin, what is your name? ").title().strip()
+
+#Adventure starts
+print("")
+
+# Encounter 1 - The Road
+
+# Encounter 2 - The Bridge
+
+# Encounter 3 - The Bear
+
+# Encounter 4 - The Boat
+
+# Encounter 5 - The Island
+
+# Encounter 6 - The Cave
+
+# Encounter 7 - The Hidden Passage
+
+# Congratulations! You Survived!
